04963a4e64fe8b9390a1dd41b4204f3c1e12b01e
Peter <phansen@physics.umn.edu>
2014-02-13 18:47:06 +0100

    vectorfit uses first 5 layers only

--- PFCalEE/analysis/test/vectorfit.cpp
+++ PFCalEE/analysis/test/vectorfit.cpp
@@ -13,6 +13,7 @@
 #include "TCanvas.h"
 #include "TStyle.h"
 #include "TMath.h"
+#include "TFitResult.h"
 
 #include "HGCSSSimHit.hh"
 #include "HGCSSParameters.hh"
@@ -31,10 +32,11 @@ double getWeight(unsigned layer,std::string aVersion){
 
 int main(int argc, char** argv){//main  
 
-  if (argc < 3) {
+  if (argc < 4) {
     std::cout << " Usage: " 
       << argv[0] << " <nEvts to process (0=all)>"
       << " <input_dir>" 
+      << " <angle>" 
       << " <optional: debug (default=0)>"
       << std::endl;
     return 1;
@@ -43,20 +45,19 @@ int main(int argc, char** argv){//main
   const unsigned pNevts = atoi(argv[1]);
   const std::string inDir = argv[2];
   bool debug = false;
-  if (argc >3) debug = atoi(argv[3]);
+  std::string angle(argv[3]);
+  if (argc >4) debug = atoi(argv[4]);
 
   TFile *outputFile = TFile::Open("vectorfit.root","RECREATE");
 
   const unsigned nLayers = N_LAYERS;
 
-  //unsigned genEn[]={5,10,25,50,75,100,150,200,300,500};
-  unsigned genEn[]={10};
+  unsigned genEn[]={5,10,25,50,75,100,150};
+  //unsigned genEn[]={10};
   const unsigned nGenEn=sizeof(genEn)/sizeof(unsigned);
 
-  TH2F *p_xy[nGenEn][nLayers];
-  TH3F *p_xyz[nGenEn];
-  TGraph * xgraph[nGenEn];
-  TGraph * ygraph[nGenEn];
+  TH2F *p_angle[nGenEn];
+
 
   double Emax[nGenEn];
 
@@ -67,17 +68,13 @@ int main(int argc, char** argv){//main
 
 
     std::ostringstream lName;
-    for (unsigned iL(0); iL<nLayers; ++iL){
-      lName << "p_xy_" << genEn[iE] << "_" << iL;
-      p_xy[iE][iL] = new TH2F(lName.str().c_str(),";x(mm);y(mm)",80,-100,100,80,-100,100);
-      lName.str("");
-    }
-    lName << "p_xyz_" << genEn[iE];
-    p_xyz[iE] = new TH3F(lName.str().c_str(),";x(mm);y(mm)",80,-100,100,80,-100,100,nLayers,0,nLayers);
+
+    lName << "p_angle_" << genEn[iE];
+    p_angle[iE] = new TH2F(lName.str().c_str(),";x angle(rad);y angle(rad)",100,-.5,.5,100,-.5,.5);
     lName.str("");
 
     std::ostringstream input;
-    input << inDir << genEn[iE] << "_0.250" << "/PFcal.root";
+    input << inDir << genEn[iE] << "_" << angle << "/PFcal.root";
     TFile *inputFile = TFile::Open(input.str().c_str());
 
     if (!inputFile) {
@@ -127,21 +124,33 @@ int main(int argc, char** argv){//main
 
     std::cout << "- Processing = " << nEvts  << " events out of " << lTree->GetEntries()/nLayers << std::endl;
 
-    unsigned nPoints = 0;
-    float xavg[nLayers];
-    float yavg[nLayers];
-    float xerr[nLayers];
-    float yerr[nLayers];
-    float layers[nLayers];
-    for (unsigned ievt(0); ievt<nEvts*nLayers; ++ievt){//loop on entries
+
+    lTree->GetEntry(0);
+    float curEvent(event);
+
+    TH2F *p_xy[nEvts][nLayers];
+    float sumw2[nEvts][nLayers];
+    for (unsigned i(0); i<nEvts; ++i){
+        for (unsigned iL(0); iL<nLayers; ++iL){
+            lName << "p_xy_" << i << "_" << iL;
+            p_xy[i][iL] = new TH2F(lName.str().c_str(),";x(mm);y(mm)",80,-100,100,80,-100,100);
+            lName.str("");
+
+            sumw2[i][iL] = 0;
+        }
+    }
+
+    for (unsigned ievt(0); ievt < nEvts*nLayers; ++ievt){//loop on entries
+
+        lTree->GetEntry(ievt);
+
+
       if (ievt%(nLayers*100) == 0) std::cout << "... Processing event: " << ievt/nLayers << std::endl;
 
-      lTree->GetEntry(ievt);
 
       unsigned layer = volNb;
       if (debug) std::cout << "... Processing layer " << layer << " with " << (*hitvec).size() << " simhits." << std::endl;
 
-      bool hits = false;
       for (unsigned iH(0); iH<(*hitvec).size(); ++iH){//loop on hits
         HGCSSSimHit lHit = (*hitvec)[iH];
         lHit.layer(layer);
@@ -154,110 +163,117 @@ int main(int argc, char** argv){//main
         }
 	double weightedE = lHit.energy()*getWeight(layer,inDir);
         if (weightedE > Emax[iE]) Emax[iE] = weightedE ;
-        p_xy[iE][layer]->Fill(posx,posy,weightedE);
-        p_xyz[iE]->Fill(posx,posy,layer,weightedE);
-        if(weightedE) hits = true;
+        if(weightedE) 
+        {
+            p_xy[int(event)][layer]->Fill(posx,posy,weightedE);
+            sumw2[int(event)][layer]+=TMath::Power(weightedE,2);
+        }
       }//loop on hits
-      if(hits)
-      {
-        layers[nPoints]=float(layer);
-        xavg[nPoints]=p_xy[iE][layer]->GetMean(1);
-        yavg[nPoints]=p_xy[iE][layer]->GetMean(2);
-        //xerr[nPoints]=p_xy[iE][layer]->GetRMS(1);
-        //yerr[nPoints]=p_xy[iE][layer]->GetRMS(2);
-        float clayer = 0.25f;
-        float cint = 0.0f;
-        float crms = 1.0f;
-        xerr[nPoints]=TMath::Sqrt(TMath::Power(layer*clayer,2)+
-            TMath::Power(p_xy[iE][layer]->GetRMS(1)*crms,2) +
-            TMath::Power(p_xy[iE][layer]->Integral()*cint,2));
-        yerr[nPoints]=TMath::Sqrt(TMath::Power(layer*clayer,2)+
-            TMath::Power(p_xy[iE][layer]->GetRMS(2)*crms,2) +
-            TMath::Power(p_xy[iE][layer]->Integral()*cint,2));
-
-        std::cout << "nPoints: " << nPoints;
-        std::cout << " xavg[nPoints]: " <<  xavg[nPoints];
-        std::cout << " yavg[nPoints]: " << yavg[nPoints];
-        std::cout << " xerr[nPoints]: " << xerr[nPoints];
-        std::cout << " yerr[nPoints]: " << yerr[nPoints];
-        std::cout << std::endl;
-
-        nPoints++;
-      }
 
     }//loop on entries
 
-    float * layererr = 0;
-    xgraph[iE] = new TGraphErrors(nPoints,layers,xavg,layererr,xerr);
-    ygraph[iE] = new TGraphErrors(nPoints,layers,yavg,layererr,yerr);
+    TGraph * xgraph;
+    TGraph * ygraph;
+    for (unsigned i(0); i<nEvts; ++i)
+    {
+        if (i%100 == 0) std::cout << "... Fitting event: " << i << std::endl;
+        int nPoints = -1;
+        int nFitPoints = 5;
+        float xavg[nLayers];
+        float yavg[nLayers];
+        float xerr[nLayers];
+        float yerr[nLayers];
+        float layers[nLayers];
+        for (int iL(0); iL<nLayers; ++iL)
+        {
+            if(p_xy[i][iL]->GetEntries() > 0)
+            {
+                nPoints++;
+                //std::cout << "supposedly valid: " << layers[iL] << ":" << xavg[iL] << ":" << xerr[iL] << std::endl;;
+            }
+            else continue;
 
-    std::cout << " -- max energy " << Emax[iE] << std::endl;
+            layers[nPoints]=float(iL);
+            xavg[nPoints]=p_xy[i][iL]->GetMean(1);
+            yavg[nPoints]=p_xy[i][iL]->GetMean(2);
+
+            //std::cout << sumw2[i][iL]  << ":"
+            //          << p_xy[i][iL]->GetSumOfWeights() << ":"
+            //          << p_xy[i][iL]->Integral() << ":"
+            //          << p_xy[i][iL]->GetEntries() << ":"
+            //          << std::endl;
+            xerr[nPoints]=200.0f/(80.0f*TMath::Sqrt(12))*TMath::Sqrt(sumw2[i][iL]/TMath::Power(p_xy[i][iL]->Integral(),2));
+            yerr[nPoints]=xerr[nPoints];
+
+            if(nPoints <= nFitPoints && xerr[nPoints]<=0.0 )
+            {
+                std::cout << "where are the zeros: " << layers[nPoints] << ":" << xavg[nPoints] << ":" << xerr[nPoints] << std::endl;
+            }
+
+
+            //xerr[nPoints]=200.0f/(80.0f*TMath::Sqrt(12))*TMath::Sqrt(;
+            //yerr[nPoints]=200.0f/(80.0f*TMath::Sqrt(12));
+
+
+
+        }
+        float * layererr = 0;
+
+        if(nPoints >1)
+        {
+            TCanvas * c = new TCanvas("asalkfj","asfka");
+
+            xgraph = new TGraphErrors(nPoints,layers,xavg,layererr,xerr);
+            xgraph->Draw("AP");
+            xgraph->Fit("pol1","Q","",0,nFitPoints);
+
+            TF1 * fit = xgraph->GetFunction("pol1");
+            float xangle =  fit->Derivative(0)/6.80f;
+
+            std::ostringstream title;
+            title << "angle: " << xangle;
+            xgraph->SetTitle(title.str().c_str());
+
+            if(event<10)
+            {
+                std::ostringstream saveName;
+                saveName << "PLOTS/Fit_" << i << "_" << genEn[iE] << "GeV" << ".png";
+                c->SaveAs(saveName.str().c_str());
+            }
+            delete c;
+
+
+
+            ygraph = new TGraphErrors(nPoints,layers,yavg,layererr,yerr);
+            ygraph->Fit("pol1","Q","",0,nFitPoints);
+            fit = ygraph->GetFunction("pol1");
+            float yangle =  fit->Derivative(0)/6.80f;
+
+            p_angle[iE]->Fill(xangle,yangle);
+        }
+        if(i == 9) std::cout << "alskaf" <<  p_xy[i][0]->GetEntries() << std::endl;
+    }
 
   }//loop on energies
 
-  TCanvas *mycAll = new TCanvas("mycAll","mycAll",1);
   TCanvas *myc = new TCanvas("myc","myc",1);
-  mycAll->Divide(5,6);
 
-  gStyle->SetOptStat(0);
+  //gStyle->SetOptStat(0);
 
   std::ostringstream saveName;
   for (unsigned iE(0); iE<nGenEn; ++iE){
-    for (unsigned iL(0); iL<nLayers; ++iL){
-      mycAll->cd(iL+1);
-      p_xy[iE][iL]->SetMaximum(Emax[iE]);
-      p_xy[iE][iL]->Draw("colz");
       myc->cd();
-
       std::ostringstream title;
-      std::string pad="";
-      if(iL < 10) pad = "0";
-      title << "Layer " << pad << iL;
-      p_xy[iE][iL]->SetTitle(title.str().c_str());
-      p_xy[iE][iL]->Draw("colz");
+      title << "Energy  " << genEn[iE] ;
+      p_angle[iE]->SetTitle(title.str().c_str());
+      p_angle[iE]->Draw("colz");
       myc->Update();
       saveName.str("");
-      saveName << "PLOTS/xySimHits_layer" << pad << iL << "_" << genEn[iE] << "GeV";
+      saveName << "PLOTS/angle_hist_" << genEn[iE] << "GeV";
       myc->Print((saveName.str()+".png").c_str());
       myc->Print((saveName.str()+".pdf").c_str());
       outputFile->cd();
-      p_xy[iE][iL]->Write();
-    }
-    p_xyz[iE]->Write();
-    saveName.str("");
-    saveName << "PLOTS/xySimHits_" << genEn[iE] << "GeV";
-    mycAll->Update();
-    mycAll->Print((saveName.str()+".png").c_str());
-    mycAll->Print((saveName.str()+".pdf").c_str());
-
-    TCanvas * c = new TCanvas("xgraph","xgraph");
-    xgraph[iE]->Draw("AP");
-    xgraph[iE]->Fit("pol2");
-
-    TF1 * fit = xgraph[iE]->GetFunction("pol2");
-    std::ostringstream title;
-    title << "x slope: " << fit->Derivative(0);
-    xgraph[iE]->SetTitle(title.str().c_str());
-
-    saveName.str("");
-    saveName << "PLOTS/xgraph_" << genEn[iE] << "GeV";
-    c->Print((saveName.str()+".png").c_str());
-    delete c;
-
-    c = new TCanvas("ygraph","ygraph");
-    ygraph[iE]->SetTitle("Y graph");
-    ygraph[iE]->Draw("AP");
-    ygraph[iE]->Fit("pol2");
-
-    fit = ygraph[iE]->GetFunction("pol2");
-    title.str("");
-    title << "y slope: " << fit->Derivative(0);
-    ygraph[iE]->SetTitle(title.str().c_str());
-
-    saveName.str("");
-    saveName << "PLOTS/ygraph_" << genEn[iE] << "GeV";
-    c->Print((saveName.str()+".png").c_str());
-    delete c;
+      p_angle[iE]->Write();
   }
 
   outputFile->Write();
